cbd12f6a2a31d3156d0a9b8b8272b7aa519155fd
Adi07-Nerd <adiyadav2306@gmail.com>
2024-05-20 16:16:23 +0530

    Basic change to continue

---
+++ src/app/third-chapter/third-chapter.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ThirdChapterRoutingModule } from './third-chapter-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ThirdChapterRoutingModule
+  ]
+})
+export class ThirdChapterModule { }
